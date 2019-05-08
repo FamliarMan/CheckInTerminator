236f5f75e0d34a02d8229118c923043eebb47a0c
longyi <longyi@2dfire.com>
2019-05-08 19:49:21 +0800

    准备编写列表页

--- app/src/main/java/com/jianglei/checkinterminator/TaskListActivity.kt
+++ app/src/main/java/com/jianglei/checkinterminator/TaskListActivity.kt
@@ -1,12 +1,24 @@
 package com.jianglei.checkinterminator
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.classic.adapter.BaseAdapterHelper
+import com.classic.adapter.CommonAdapter
+import com.jianglei.girlshow.storage.TaskRecord
 
 class TaskListActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_list)
+    }
+
+    class TaskAdapter(context:Context,tasks:List<TaskRecord>):
+        CommonAdapter<TaskRecord>(context,R.layout.,tasks) {
+        override fun onUpdate(helper: BaseAdapterHelper?, item: TaskRecord?, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
     }
 }
--- app/src/main/java/com/jianglei/checkinterminator/storage/TaskRecord.kt
+++ app/src/main/java/com/jianglei/checkinterminator/storage/TaskRecord.kt
@@ -27,56 +27,63 @@ data class TaskRecord(
      * 任务开始具体时间，如 16:00
      */
     @ColumnInfo(name = "startTime")
-    var startTime:String,
+    var startTime: String,
 
     /**
      * 星期几生效，用"，"隔开，如星期一，星期二
      */
     @ColumnInfo(name = "week")
-    var week:String,
+    var week: String,
 
 
     /**
      * 位置中心的经度
      */
     @ColumnInfo(name = "Lng")
-    var Lng:String,
+    var Lng: String,
 
     /**
      * 位置中心的维度
      */
-    @ColumnInfo(name="Lat")
-    var Lat:String
+    @ColumnInfo(name = "Lat")
+    var Lat: String,
+
+    /**
+     * 上一次完成任务时间
+     */
+    var lastDoneTime: Long
 
 
-):Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
+) : Parcelable {
+    constructor(source: Parcel) : this(
+        source.readInt(),
+        source.readString(),
+        source.readInt(),
+        source.readString(),
+        source.readString(),
+        source.readString(),
+        source.readString(),
+        source.readLong()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeInt(id)
+        writeString(name)
+        writeInt(enabled)
+        writeString(startTime)
+        writeString(week)
+        writeString(Lng)
+        writeString(Lat)
+        writeLong(lastDoneTime)
     }
 
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun describeContents(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    companion object CREATOR : Parcelable.Creator<TaskRecord> {
-        override fun createFromParcel(parcel: Parcel): TaskRecord {
-            return TaskRecord(parcel)
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<TaskRecord> = object : Parcelable.Creator<TaskRecord> {
+            override fun createFromParcel(source: Parcel): TaskRecord = TaskRecord(source)
+            override fun newArray(size: Int): Array<TaskRecord?> = arrayOfNulls(size)
         }
-
-        override fun newArray(size: Int): Array<TaskRecord?> {
-            return arrayOfNulls(size)
-        }
     }
-
 }
