b1793a3136300849a039b6a5b756e6554e982e60
longyi <longyi@2dfire.com>
2019-05-10 17:26:25 +0800

    搞定控件

---
+++ app/src/main/java/com/jianglei/checkinterminator/TaskEditActivity.kt
@@ -0,0 +1,12 @@
+package com.jianglei.checkinterminator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_edit)
+    }
+}
---
+++ app/src/main/java/com/jianglei/checkinterminator/util/DialogUtils.kt
@@ -0,0 +1,49 @@
+package com.jianglei.checkinterminator.util
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import com.jianglei.checkinterminator.R
+
+/**
+ *@author longyi created on 19-3-25
+ */
+class DialogUtils {
+    companion object {
+        /**
+         * 显示提示对话框
+         */
+        fun showTipDialog(context: Context?, msg: String?) {
+            if (context == null || (context is Activity && context.isFinishing)) {
+                return
+            }
+            val builder = AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.dialog_tip))
+                .setMessage(msg)
+                .setCancelable(true)
+            builder.create().show()
+        }
+
+        fun showClickDialog(
+            context: Context?,
+            msg: String?,
+            rightText: String,
+            listener: DialogInterface.OnClickListener?
+        ) {
+            if (context == null || (context is Activity && context.isFinishing)) {
+                return
+            }
+
+            val builder = AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.dialog_tip))
+                .setMessage(msg)
+                .setCancelable(true)
+                .setNegativeButton(context.getString(R.string.cancel), null)
+                .setPositiveButton(rightText, listener)
+            builder.create().show()
+        }
+
+
+    }
+}
